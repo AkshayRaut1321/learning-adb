340700ec994725fc78e2e172b60be16b7032a593
akshay.raut@gep.com <akshay.raut@gep.com>
2022-11-06 15:53:37 +0000

    Learning DataFrame reader completed

--- formula1/01. ingestion/01. ingest_circuits_file.py
+++ formula1/01. ingestion/01. ingest_circuits_file.py
@@ -57,18 +57,79 @@ circuits_df.select("*", "circuitsId").show()
 
 # COMMAND ----------
 
+### Method 1:
 circuits_df.select("circuitsId", "circuitRef", "name", "location", "country", "lat", "lng", "alt").show()
 
 # COMMAND ----------
 
+### Method 2:
 circuits_df.select(circuits_df.circuitsId, circuits_df.circuitRef, circuits_df.name, circuits_df.location, circuits_df.country, circuits_df.lat, circuits_df.lng, circuits_df.alt).show()
 
 # COMMAND ----------
 
+### Method 3:
 circuits_df.select(circuits_df["circuitsId"], circuits_df["circuitRef"], circuits_df["name"], circuits_df["location"], circuits_df["country"], circuits_df["lat"], circuits_df["lng"], circuits_df["alt"]).show()
 
 # COMMAND ----------
 
+### Method 4:
+
 from pyspark.sql.functions import col
 
 circuits_df.select(col("circuitsId").alias("circuits_id"), col("circuitRef"), col("name"), col("location"), col("country"), col("lat"), col("lng"), col("alt")).show()
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select(col("circuitsId"), col("circuitRef"), col("name"), col("location"), col("country"), col("lat"), col("lng"), col("alt"))
+
+# COMMAND ----------
+
+### Method 5:
+
+circuits_renamed_df = circuits_selected_df.withColumnRenamed("circuitsId", "circuits_id") \
+.withColumnRenamed("circuitRef", "circuit_ref") \
+.withColumnRenamed("name", "name") \
+.withColumnRenamed("location", "location") \
+.withColumnRenamed("country", "country") \
+.withColumnRenamed("lat", "latitude") \
+.withColumnRenamed("lng", "longitude") \
+.withColumnRenamed("alt", "altitude")
+
+# COMMAND ----------
+
+circuits_renamed_df.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Add new column using DataFrame.withColumn(<column_name>, column_object) function
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Add new column by passing a column object
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+circuits_renamed_df.withColumn("ingestion_date", current_timestamp()).show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Add new column by passing a literal value
+
+# COMMAND ----------
+
+from pyspark.sql.functions import lit
+
+circuits_selected_df.withColumn("env", lit("Development")).show()
+
+# COMMAND ----------
+
+circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+display(circuits_final_df)

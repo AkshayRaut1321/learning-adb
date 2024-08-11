9bb87328fc92a6691bd325b8c43b0a86a362fd0c
AkshayRaut1321 <AkshayRaut1321>
2024-08-11 18:16:22 +0000

    test git commit

--- playground/05.02 sql_objects_demo_managed_tables.sql
+++ playground/05.02 sql_objects_demo_managed_tables.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 -- MAGIC %md
--- MAGIC ##### Lesson objectives
+-- MAGIC #### Lesson objectives
 -- MAGIC 1. Create managed table using Python
 -- MAGIC 2. Create managed table using SQL
 -- MAGIC 3. Effect of dropping a managed table
